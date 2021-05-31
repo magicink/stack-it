04bba4093ba6bec8eecd3cbb1e303521c2ff4972
Brandon Tom <magicink@gmail.com>
2021-05-31 07:44:35 -0700

    add event delegates to the app manager

--- Assets/Scripts/Managers/AppManager.cs
+++ Assets/Scripts/Managers/AppManager.cs
@@ -1,19 +1,35 @@
-using UnityEngine;
+using System;
+using States;
 
 namespace Managers
 {
     public class AppManager : PyxlMedia.States.Controllers.StateController
     {
-        // Start is called before the first frame update
-        void Start()
+        public static event Action GameStart = delegate {  };
+        public static event Action GameEnd = delegate {  };
+        public static event Action DemoStart = delegate {  };
+
+        private void OnGameStart()
         {
-        
+            SetState(GameStates.Playing);
+            GameStart();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnGameEnd()
         {
-        
+            SetState(GameStates.Idle);
+            GameEnd();
+        }
+
+        private void OnDemoStart()
+        {
+            SetState(GameStates.Demo);
+            DemoStart();
+        }
+
+        private void Awake()
+        {
+            SetState(GameStates.Idle);
         }
     }
 }
---
+++ Assets/Tests/PlayMode/Managers/AppManagerTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using Managers;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests.PlayMode.Managers
+{
+    public class AppManagerTests
+    {
+        [UnityTest]
+        public IEnumerator HasCorrectInitialState()
+        {
+            var gameObject = new GameObject();
+            gameObject.AddComponent<AppManager>();
+            var appManager = gameObject.GetComponent<AppManager>();
+            Assert.That(appManager.CurrentState, Is.InstanceOf<PyxlMedia.States.State>());
+            yield return null;
+        }
+    }
+}
