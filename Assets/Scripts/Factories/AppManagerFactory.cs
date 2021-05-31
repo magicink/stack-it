5b6f8af3320c8b0ca57f0e76532870c222adde44
Brandon Tom <magicink@gmail.com>
2021-05-30 19:22:59 -0700

    initial commit

---
+++ Assets/Scripts/Factories/AppManagerFactory.cs
@@ -0,0 +1,8 @@
+using Managers;
+
+namespace Factories
+{
+    public class AppManagerFactory : PyxlMedia.States.Factories.StateControllerFactory<AppManager>
+    {
+    }
+}
---
+++ Assets/Scripts/Managers/AppManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Managers
+{
+    public class AppManager : PyxlMedia.States.Controllers.StateController
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
