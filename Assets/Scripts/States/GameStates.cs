d968ae82bf46ea8e41f04836b6b27d354ff2995b
Brandon Tom <magicink@gmail.com>
2021-05-31 07:09:19 -0700

    add game states

---
+++ Assets/Scripts/States/GameStates.cs
@@ -0,0 +1,14 @@
+﻿namespace States
+{
+    
+    public class GameStates
+    {
+        public class PlayingState : PyxlMedia.States.State {}
+        public class IdleState : PyxlMedia.States.State {}
+        public class DemoState : PyxlMedia.States.State {}
+
+        public static PlayingState Playing = PyxlMedia.States.Factories.StateFactory<PlayingState>.Instance;
+        public static IdleState Idle = PyxlMedia.States.Factories.StateFactory<IdleState>.Instance;
+        public static DemoState Demo = PyxlMedia.States.Factories.StateFactory<DemoState>.Instance;
+    }
+}
---
+++ Assets/Tests/PlayMode/States/GameStatesTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using States;
+
+namespace Tests.PlayMode.States
+{
+    public class GameStatesTests
+    {
+        [Test]
+        public void ExposesStates()
+        {
+            Assert.That(GameStates.Playing, Is.InstanceOf<PyxlMedia.States.State>());
+            Assert.That(GameStates.Idle, Is.InstanceOf<PyxlMedia.States.State>());
+            Assert.That(GameStates.Demo, Is.InstanceOf<PyxlMedia.States.State>());
+        }
+    }
+}
