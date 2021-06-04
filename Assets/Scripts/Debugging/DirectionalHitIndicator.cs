5cc120698f070741dfdbac5af917830778046c77
Brandon Tom <magicink@gmail.com>
2021-06-04 05:32:21 -0700

    added new light

--- Assets/Scripts/Debugging/DirectionalHitIndicator.cs
+++ Assets/Scripts/Debugging/DirectionalHitIndicator.cs
@@ -13,18 +13,18 @@ namespace Debugging
 
         private void OnDrawGizmos()
         {
-            var ray = new Ray(transform.position, Vector3.down);
-            if (!Physics.Raycast(ray, out var hit)) return;
-            Handles.color = rayColor;
-            Handles.zTest = UnityEngine.Rendering.CompareFunction.LessEqual;
-            var position = transform.position;
-            Handles.DrawLine(position, hit.point);
-            Handles.DrawWireDisc(hit.point, hit.normal, size);
-
-            Handles.color = indicatorColor;
-            Handles.zTest = UnityEngine.Rendering.CompareFunction.Greater;
-            Handles.DrawLine(position, hit.point);
-            Handles.DrawWireDisc(hit.point, hit.normal, size * 1.25f);
+            // var ray = new Ray(transform.position, Vector3.down);
+            // if (!Physics.Raycast(ray, out var hit)) return;
+            // Handles.color = rayColor;
+            // Handles.zTest = UnityEngine.Rendering.CompareFunction.LessEqual;
+            // var position = transform.position;
+            // Handles.DrawLine(position, hit.point);
+            // Handles.DrawWireDisc(hit.point, hit.normal, size);
+            //
+            // Handles.color = indicatorColor;
+            // Handles.zTest = UnityEngine.Rendering.CompareFunction.Greater;
+            // Handles.DrawLine(position, hit.point);
+            // Handles.DrawWireDisc(hit.point, hit.normal, size * 1.25f);
         }
     }
 }
